98a1da1d28531a2a3f53e7c542e859222575468a
momeppkt <peeranut32@gmail.com>
2024-12-13 19:45:39 +0700

    chore: add .env example

--- configs/env.go
+++ configs/env.go
@@ -12,3 +12,8 @@ func LoadEnv() {
 		log.Println("No .env file found, using system environment variables")
 	}
 }
+
+// INFO: example .env
+// DB_DSN=host=localhost user=your_user password=your_password dbname=your_db port=5432 sslmode=disable
+// APP_PORT=8080
+// JWT_SECRET=supersecretkey
